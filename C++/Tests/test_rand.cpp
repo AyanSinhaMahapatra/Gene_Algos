a702daac746020a6c2eeddba5f36f390bb3155bc
Ayan Sinha Mahapatra <ayansmahapatra@gamil.com>
2018-03-08 04:02:09 +0530

    Crossover Testing Code Commit 1

--- C++/Tests/test_rand.cpp
+++ C++/Tests/test_rand.cpp
@@ -4,28 +4,80 @@
 #include<ctime>
 using namespace std;
 
-int check_array(int rand_gen,int *array_random,int size)
+void random_generate_array(int *array,int length) // Of Length "length" half 1 half 0
 {
-	int flag = 1; 
-	cout<<"Size = "<<size<<endl;
-	for(int i=1;i<=size;i++)
+	srand(time(NULL));
+	
+	for(int i=0;i<=length;i++)
+		array[i]=0;
+
+	int rand_gen=0;
+
+	for(int i=1;i<=(length/2);i++)
 	{
-		if(array_random[i]==rand_gen)
-			flag=0;
+		rand_gen = rand() % length;
+		rand_gen += 1;
+
+		while(array[rand_gen]==1)
+		{
+			rand_gen = rand() % length;
+			rand_gen += 1;
+		}
+
+		if(array[rand_gen]!=1)
+			array[rand_gen]=1;
+		else
+			i--;
 	}
-	return flag;
+	return;
 }
 
 int main()
 {
-	 int array_int[7];
-    for(int i=1;i<7;i++)
-    {
-    	array_int[i]=i+1;
-    }
-    for(int i=1;i<7;i++)
-    {
-    	cout<<i+1<<" = "<<check_array(i+1,array_int,6)<<endl;
-    }
+	int length = 200;
+	int array[length+1];
+	random_generate_array(array,length);
+
+	int count = 0;
+	for(int i=1;i<=length;i++)
+		if(array[i]==1)
+			count++;
+
+	if(count!=(length/2))
+		cout<<count<<endl;
+	else 
+		cout<<"Cool"<<endl;
+
+	for(int i=1;i<=length;i++)
+		//cout<<array[i]<<" ";
+
 	return 0;
 }
+
+/* FOR RANDOM NUMBER RANGE
+int main()
+{
+
+	srand(time(NULL));
+	int highest = 0;
+	int lowest = 1000;
+	int rand_gen = 0;
+	int length = 40;
+	int add = 1;
+
+    for(int i=1;i<1000000;i++)
+    {
+    	rand_gen = rand() % length;
+    	rand_gen += add;
+
+    	if(rand_gen<lowest)
+    		lowest = rand_gen;
+    	if(rand_gen>highest)
+    		highest = rand_gen;
+    }
+
+    cout<<highest<<" -- "<<lowest<<endl;
+
+	return 0;
+}
+*/
--- C++/crossover_methods.cpp
+++ C++/crossover_methods.cpp
@@ -1,4 +1,38 @@
-void simple_crossover_reproduction(int *parent1,int *parent2,int *offspring1,int *offspring2,int length)
+#include<iostream>
+#include<cmath>
+#include<algorithm>
+#include<ctime>
+
+
+	int case_cross = 1; 
+	int array_length = 10;
+	int crossover_lenght = 4;
+	int check_array[1025];
+	int level_limit = 5;
+	int no_of_rec = 10;
+
+void random_generate_array(int *array,int length);
+void apply_crossover(int *array);
+
+// All types of Crossover Reproduction Functions
+void one_point_crossover(int *parent1,int *parent2,int *offspring1,int *offspring2,int length);
+
+int main()
+{
+	int array[array_length+1];
+	random_generate_array(array,array_length);
+
+	recursive_trav(array,1);
+
+	check_and_print_results(array);
+
+}
+
+
+
+// All types of Crossover Reproduction Functions
+
+void one_point_crossover(int *parent1,int *parent2,int *offspring1,int *offspring2,int length)
 {
 	int rand_boundary = length-2 ;
 	//Left To Right
@@ -20,3 +54,89 @@ void simple_crossover_reproduction(int *parent1,int *parent2,int *offspring1,int
 	}
 }
 
+// Other Functions 
+
+void recursive_trav(int *array,int rec_level)
+{
+	if(rec_level == level_limit)
+		return;
+
+	apply_crossover(array);
+	check_history(array);
+
+	for(int i=1;i<=no_of_rec;i++)
+		recursive_trav(array,rec_level+1);
+
+}
+
+void check_history(int *array)
+{
+	int number = 0;
+	number = binary_to_dec(array);
+	
+	check_array[number]++;
+
+	return;
+}
+
+int binary_to_dec(int *array)
+{
+	int sum = 0;
+
+	return sum;
+}
+
+void apply_crossover(int *array)
+{
+	int parents_index[4];
+	int parent1[p_length];
+	int parent2[p_length];
+	int offspring1[p_length];
+	int offspring2[p_length];
+
+	choose_parents(parents_index);
+	
+	copy_parents_from_array(array,parent1,parent2,parents_index);
+
+	switch(case_cross)
+	{
+		case 1 : 
+				break;
+		case 2 : 
+				break;
+	}
+
+	copy_offsprings_to_array(array,offspring1,offspring2,parents_index);
+
+	return;
+}
+
+void random_generate_array(int *array,int length) // Of Length "length" half 1 half 0
+{
+	srand(time(NULL));
+	
+	for(int i=0;i<=length;i++)
+		array[i]=0;
+	for(int i=0;i<=1024;i++)
+		check_array[i]=0;
+
+	int rand_gen=0;
+
+	for(int i=1;i<=(length/2);i++)
+	{
+		rand_gen = rand() % length;
+		rand_gen += 1;
+
+		while(array[rand_gen]==1)
+		{
+			rand_gen = rand() % length;
+			rand_gen += 1;
+		}
+
+		if(array[rand_gen]!=1)
+			array[rand_gen]=1;
+		else
+			i--;
+	}
+	return;
+} 

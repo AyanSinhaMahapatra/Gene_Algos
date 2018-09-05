6067473adce0d13098d76d5df812494879c05eec
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2018-09-05 21:49:13 +0530

    Archive Initial Tests Passed

--- C++/tree_test_nonrevisit.cpp
+++ C++/tree_test_nonrevisit.cpp
@@ -3,9 +3,13 @@
 #include<cmath>
 #include<algorithm>
 #include<ctime>
+#include <mcheck.h>
 #include<Eigen/Dense> //Linear Algebra Library
 using namespace std;
 using namespace Eigen; 
+
+
+
 
 // 0 - Left, 1 - Right 
 // data - At which Depth the Node is in the main Tree
@@ -19,13 +23,17 @@ struct treenode
     struct treenode* parent;
 };
 
-	struct treenode *cur_loc;
+	struct treenode *archive;
 	long long int pop_counter = 0;
 	double rand_find_thr = 0.5;
+	int num_arr = 8;
 
 
 // Helper Functions
 void random_generate(Eigen::VectorXd& array);
+void random_generate_arrays(Eigen::MatrixXd& arrays);
+int is_okay(Eigen::VectorXd& array);
+void no_op(enum mcheck_status status) {}
 
 // Tree Functions
 struct treenode* newNode(int data);
@@ -44,11 +52,51 @@ void stray_rec(struct treenode *temp);
 int main()
 {
 	srand(time(NULL));
+	mcheck(&no_op);
 	
 	struct treenode current;
-    cur_loc = &current;
-	cur_loc = newNode(0);
+    archive = &current;
+	archive = newNode(0);
 
+	Eigen::VectorXd array(116);
+	random_generate(array);
+
+	Eigen::MatrixXd arrays(num_arr+1,116);
+    random_generate_arrays(arrays);
+
+    array = arrays.row(1);
+    for(int i=1;i<=num_arr;i++)
+    {
+    	cout<<i<<" no Loop"<<endl;
+    	array(115) = 1 - array(115);
+    	if(i%2==0)
+    		array(114) = 1 - array(114);
+    	if(i%4==0)
+    		array(113) = 1 - array(113);
+    	insert_array(archive,array);
+
+    	for(int j=1;j<=115;j++)
+            cout<<array(j);
+        cout<<endl;
+    }
+
+    cout<<"Hi"<<endl;
+
+    array = arrays.row(1);
+    for(int i=1;i<=num_arr;i++)
+    {
+    	array(115) = 1 - array(115);
+    	if(i%2==0)
+    		array(114) = 1 - array(114);
+    	if(i%4==0)
+    		array(113) = 1 - array(113);
+    	cout<<is_present(archive,array)<<endl;
+    }
+
+    cout<<"Population == "<<population(archive)<<endl;
+
+    array = arrays.row(1);
+    follow_print(archive,array);
 
 	return 0;
 }
@@ -74,7 +122,7 @@ void insert_array(struct treenode *root,Eigen::VectorXd& array) //Done
 {
 	if(root == NULL)
 	{
-		cout<<"Tree Empty"<<endl;
+		cout<<"Invalid Root"<<endl;
 		return;
 	}
 
@@ -123,17 +171,24 @@ void insert_array(struct treenode *root,Eigen::VectorXd& array) //Done
 
 	if (temp == NULL)
 	{
-		cout<<"Insert_Array Null encountered "<<endl;
+		cout<<"Insert_Array -> Tree Overflow "<<endl;
 		return;
 	}
 
 	if(temp->left != NULL && temp->left->status == 1)
 		temp = temp->left;
-	else 
+	else
 		temp = temp->right;
 
-	del_rec(temp->left);
-	del_rec(temp->right);
+
+	struct treenode *left, *right;
+	left = temp->left;
+	right = temp->right;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	del_rec(left);
+	del_rec(right);
 
 	return;
 }
@@ -152,15 +207,18 @@ void follow_print(struct treenode *root,Eigen::VectorXd& array)
 
 	for(int i=1;i<=115;i++)
 	{
-		if(temp == NULL)
+		if(temp == NULL /*|| (temp->left!=NULL && temp->right!=NULL)*/)
+		{
+			cout<<"End at == "<<i-2<<endl;	
 			return;
+		}
 
 		temp_int = array(i);
 
 		if(temp->left!=NULL)
-			cout<<i<<"  LEFT TRUE   ";
+			cout<<i-1<<"  LEFT TRUE   ";
 		else
-			cout<<i<<"  LEFT FALSE   ";
+			cout<<i-1<<"  LEFT FALSE   ";
 		if(temp->right!=NULL)
 			cout<<"RIGHT TRUE"<<endl;
 		else 
@@ -279,14 +337,14 @@ void find_open(struct treenode *root,Eigen::VectorXd& array,Eigen::VectorXd& new
 
 	if(temp->left != NULL && temp->left->status == 1)
 	{
-		if(temp->right != NULL)
+		if(temp->right == NULL)
 			temp->right = newNode(temp->data + 1);
 		temp = temp -> right;
 		new_array(temp->data) = 1;
 	}
-	else if(temp->right != NULL && temp->right->status == 1)
+	else
 	{
-		if(temp->left != NULL)
+		if(temp->left == NULL)
 			temp->left = newNode(temp->data + 1);
 		temp = temp -> left;
 		new_array(temp->data) = 0;
@@ -299,14 +357,14 @@ void find_open(struct treenode *root,Eigen::VectorXd& array,Eigen::VectorXd& new
 			if(temp->left != NULL && temp->left->status == 1)
 			{
 				if( temp->right == NULL)
-					temp->right = newnode(temp->data+1);
+					temp->right = newNode(temp->data+1);
 				temp = temp->right;
 				new_array(temp->data) = 1;
 			}
 			else 
 			{
 				if( temp->left == NULL)
-					temp->left = newnode(temp->data+1);
+					temp->left = newNode(temp->data+1);
 				temp = temp->left;
 				new_array(temp->data) = 0;
 			}
@@ -315,17 +373,17 @@ void find_open(struct treenode *root,Eigen::VectorXd& array,Eigen::VectorXd& new
 		{
 			rand_find = rand() / (double)RAND_MAX;
 
-			if(rand_find<rand_find_thr)   //Left
+			if(rand_find < rand_find_thr)   //Left
 			{
 				if( temp->left == NULL)
-					temp->left = newnode(temp->data+1);
+					temp->left = newNode(temp->data+1);
 				temp = temp->left;
 				new_array(temp->data) = 0;
 			}
 			else //Right
 			{
 				if( temp->right == NULL)
-					temp->right = newnode(temp->data+1);
+					temp->right = newNode(temp->data+1);
 				temp = temp->right;
 				new_array(temp->data) = 1;
 			}
@@ -394,7 +452,7 @@ void delete_array(struct treenode *root,Eigen::VectorXd& array)
 }
 */
 
-void delete_tree(struct treenode *root) //Done
+void delete_tree(struct treenode* root) //Done
 {
 	if(root == NULL)
 	{
@@ -406,12 +464,14 @@ void delete_tree(struct treenode *root) //Done
 
 	return;	
 }
+//-------------------------------------     MAIN DELETE FUNCTION     ---------------------------------------------------------
 
-void del_rec(struct treenode *temp) //Done
+void del_rec(struct treenode* temp) //Done
 {
+	//if(temp == NULL || temp->data >115)
 	if(temp == NULL)
 		return;
-
+	
 	del_rec(temp->left);
 	del_rec(temp->right);
 	free(temp);
@@ -443,6 +503,8 @@ void pop_rec(struct treenode *temp) //Done
 	else if(temp->status == 1)
 	{
 		pop_counter += pow(2,(115 - temp->data));
+		if(temp->data != 115)
+			cout<<"Pop Data = "<<temp->data<<endl;
 		return;
 	}
 	else
@@ -488,4 +550,51 @@ void random_generate(Eigen::VectorXd& array) //Generates A Random Array which ob
     }
     array(94)= int(rand() % 2);
     array(115)= 1 - array(94);
+}
+
+void random_generate_arrays(Eigen::MatrixXd& arrays)
+{
+    Eigen::VectorXd array(116);
+    arrays.fill(0);
+
+    for(int i=1;i<=num_arr;i++)
+    {
+        random_generate(array);
+        for(int j=1;j<=115;j++)
+        {
+            arrays(i,j)=array(j);
+        }
+        array = arrays.row(i);
+        if(is_okay(array)==0)
+            cout<<"ARRAY NOT OKAY ERROR"<<endl;
+    }
+    return;
+}
+
+int is_okay(Eigen::VectorXd& array)  //Returns 1 if okay 0 if not okay
+{
+    int flag=1,total_ones=0;
+    if(array(1)!=0)
+    {
+        flag=0;
+        cout<<"Problem 1"<<endl;
+    }
+    if(array(115) != 1 - array(94))
+    {
+        flag=0;
+        cout<<"Problem 2"<<endl;
+    }
+    for(int i=2;i<=114;i++)
+    {
+        if(i==94)
+            i++;
+        if(int(array(i))==1)
+            total_ones++;
+    }
+    if(total_ones!=56)
+    {
+        flag=0;
+        cout<<"Problem 3"<<endl;
+    }
+    return flag;
 }
